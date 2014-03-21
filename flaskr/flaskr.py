e89dac154a7a5870f3b81d732f9b68e3c20f3691
Ed Paradis <legomaniac@gmail.com>
2014-03-21 00:05:36 -0700

    step 3: database connections

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -23,6 +23,20 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def get_db():
+    """opens a new database connection if there is none yet for the current app context"""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+# functions with this decorate are called every time the application context tears down
+@app.teardown_appcontext
+def close_db(error):
+    """closes the databse again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 if __name__ == '__main__':
     app.run()
 
