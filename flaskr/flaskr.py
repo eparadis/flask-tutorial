8a3dbec8c45e620f1b89bfc9c42df3f60ff2d463
Ed Paradis <legomaniac@gmail.com>
2014-03-21 00:12:20 -0700

    step 4: creating the databse

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -36,6 +36,12 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
     app.run()
